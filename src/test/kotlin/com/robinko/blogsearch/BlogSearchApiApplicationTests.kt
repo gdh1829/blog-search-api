ab08c650e16c0f3549955f8cf959fa38fae0f2ba
daehyeop.ko <daehyeop.ko@gmail.com>
2023-03-18 21:19:02 +0900

    init project

---
+++ src/test/kotlin/com/robinko/blogsearch/BlogSearchApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.robinko.blogsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogSearchApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
