d56ce01627f9d62c99f06f64847310bea478090e
daehyeop.ko <daehyeop.ko@gmail.com>
2023-03-20 01:39:44 +0900

    edit datasource with initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,19 @@
-CREATE TABLE `shedlock` (
-    `name` varchar(64) NOT NULL,
-    `lock_until` timestamp(3) NOT NULL,
-    `locked_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `locked_by` varchar(255) NOT NULL,
-    PRIMARY KEY (`name`)
-);
+
+INSERT INTO BLOGSEARCHPRIORITY (version, createdTime, updatedTime, `source`, use, priority) VALUES
+(0, now(), now(), 'KAKAO', true, 0),
+(0, now(), now(), 'NAVER', true, 1)
+;
+
+INSERT INTO KEYWORDSTATISTICS(keyword, createdTime, updatedTime, deletedTime, searchCount) VALUES
+('사과', now(), now(), null, 40),
+('제주도', now(), now(), null, 30),
+('여행', now(), now(), null, 10),
+('프로그래밍', now(), now(), null, 26),
+('코틀린', now(), now(), null, 10),
+('농구', now(), now(), null, 10),
+('브런치', now(), now(), null, 50),
+('일기', now(), now(), null, 10),
+('깃헙', now(), now(), null, 11),
+('맥북', now(), now(), null, 30),
+('전래동화', now(), now(), null, 7)
+;
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,30 @@
+-- create table BlogSearchPriority(
+--    `serverId` bigint(20) NOT NULL AUTO_INCREMENT,
+--    `version` bigint(20) NOT NULL,
+--    `createdTime` datetime NOT NULL,
+--    `updatedTime` datetime NOT NULL,
+--    `source` varchar(255) NOT NULL UNIQUE,
+--    `use` bit(1) NOT NULL,
+--    `priority` int(2) NOT NULL,
+--    PRIMARY KEY(`serverId`)
+-- );
+CREATE INDEX `BlogSearchPriority_source_idx` ON `BLOGSEARCHPRIORITY` (`source`);
+
+-- create table KeywordStatistics(
+--    `keyword` varchar(255) NOT NULL,
+--    `createdTime` datetime NOT NULL,
+--    `updatedTime` datetime NOT NULL,
+--    `deletedTime` datetime NULL,
+--    `searchCount` bigint(20) NOT NULL,
+--    PRIMARY KEY(`keyword`)
+-- );
+
+CREATE INDEX `KeywordStatistics_searchCount_idx` ON `KEYWORDSTATISTICS` (`searchCount`, `updatedTime`);
+
+-- create TABLE shedlock(
+--  name VARCHAR(64) NOT NULL,
+--  lock_until TIMESTAMP(3) NOT NULL,
+--  locked_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+--  locked_by VARCHAR(255) NOT NULL,
+--  PRIMARY KEY (name)
+-- );
